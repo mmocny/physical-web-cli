7f2da04e2a3fa33e97e4635e2d50b02e9796cf94
Michal Mocny <mmocny@gmail.com>
2016-11-17 14:54:13 -0500

    Resolve before advertising

--- main.js
+++ main.js
@@ -41,60 +41,72 @@ function resolve(urls) {
     options: { is_user_facing: false }
   });
 
-  fetch(pws_resolve_url, {
+  return fetch(pws_resolve_url, {
     method: 'POST',
     headers: GetConfigAt("pws", "resolve", "headers"),
     body
   }).then(function(response) {
     return response.json();
-  }).then(function(json) {
-    console.log(JSON.stringify(json, null, 4));
-  }).catch(function(ex) {
-    console.log('parsing failed', ex);
+  })
+  .catch(function(ex) {
+    console.error(ex);
   });
 }
 
 /******************************************************************************/
 
 function scan() {
-  require('physical-web-scan');
+  return require('physical-web-scan');
 }
 
 /******************************************************************************/
 
 function advertise(urls) {
-  console.log('Advertise', urls);
-  // TODO: try to resolve to make sure this works
+  return resolve(urls)
+    .then(function(json) {
+      console.log(JSON.stringify(json, null, 4));
 
-  const es_beacon = require('eddystone-beacon');
-  const es_url_encode = require('eddystone-url-encoding').encode;
-  const goo_gl = require('goo.gl');
-  const URL = require('url');
-
-  goo_gl.setKey(GetConfigAt("goo.gl", "api-key"));
-
-  Q.all(urls.map((url) => {
-    let short_url_promise = (URL.parse(url).hostname == 'goo.gl') ? Q(url) : goo_gl.shorten(url);
-    return short_url_promise
-      .then((short_url) => {
-        console.log(`Advertising and Watching: ${url} [ short: ${short_url} ]`);
-        es_beacon.advertiseUrl(short_url, [GetConfigAt("beacon", "options")]);
-
-        /*
-        setInterval(() => {
-          goo_gl.analytics(short_url, { projection: "FULL" })
-            .then((results) => {
-              let last2 = results["twoHours"];
-              let clicks = last2["shortUrlClicks"];
-              let browsers = (last2["browsers"] || []).map((vals) => `${vals.id}: ${vals.count}`).join(", ");
-              let platforms = (last2["platforms"] || []).map((vals) => `${vals.id}: ${vals.count}`).join(", ");
-              console.log(`${url}: ${clicks} || ${browsers} || ${platforms}`);
-            }).done();
-        }, 2000);
-        */
-      });
+      if (!('results' in json)) {
+        throw new Error('No resolved results to advertise');
+      }
     })
-  ).done();
+    .then(function() {
+      console.log('Advertise', urls);
+      // TODO: try to resolve to make sure this works
+
+      const es_beacon = require('eddystone-beacon');
+      const es_url_encode = require('eddystone-url-encoding').encode;
+      const goo_gl = require('goo.gl');
+      const URL = require('url');
+
+      goo_gl.setKey(GetConfigAt("goo.gl", "api-key"));
+
+      Q.all(urls.map((url) => {
+        let short_url_promise = (URL.parse(url).hostname == 'goo.gl') ? Q(url) : goo_gl.shorten(url);
+        return short_url_promise
+          .then((short_url) => {
+            console.log(`Advertising and Watching: ${url} [ short: ${short_url} ]`);
+            es_beacon.advertiseUrl(short_url, [GetConfigAt("beacon", "options")]);
+
+            /*
+            setInterval(() => {
+              goo_gl.analytics(short_url, { projection: "FULL" })
+                .then((results) => {
+                  let last2 = results["twoHours"];
+                  let clicks = last2["shortUrlClicks"];
+                  let browsers = (last2["browsers"] || []).map((vals) => `${vals.id}: ${vals.count}`).join(", ");
+                  let platforms = (last2["platforms"] || []).map((vals) => `${vals.id}: ${vals.count}`).join(", ");
+                  console.log(`${url}: ${clicks} || ${browsers} || ${platforms}`);
+                }).done();
+            }, 2000);
+            */
+          });
+        })
+      ).done();
+    })
+    .catch(function(ex) {
+      console.error(ex);
+    });
 }
 
 /******************************************************************************/
@@ -131,10 +143,16 @@ function main() {
 
     case 'resolve':
     case 'r':
-      return resolve(args);
+      return resolve(args)
+        .then(function(json) {
+          console.log(JSON.stringify(json, null, 4));
+        });
 
     default:
-      return resolve([command]);
+      return resolve([command])
+        .then(function(json) {
+          console.log(JSON.stringify(json, null, 4));
+        });
   }
 }
 
